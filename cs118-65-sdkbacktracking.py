f7304920606dea9a488129a6c6cd5bb4ca0ba4c0
Vytla Harsha Vardhan <71075957+harsha-vardhan-vytla@users.noreply.github.com>
2020-12-11 15:16:54 +0530

    Add files via upload

---
+++ cs118-65-sdkbacktracking.py
@@ -0,0 +1,109 @@
+import sys
+board = [
+    [3,0,6,5,0,8,4,0,0],
+    [5,2,0,0,0,0,0,0,0],
+    [0,8,7,0,0,0,0,3,1],
+    [0,0,3,0,1,0,0,8,0],
+    [9,0,0,8,6,3,0,0,5],
+    [0,5,0,0,9,0,6,0,0],
+    [1,3,0,0,0,0,2,5,0],
+    [0,0,0,0,0,0,0,7,4],
+    [0,0,5,2,0,6,3,0,0]
+]
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                sys.stdout.write(" | ")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                sys.stdout.write(str(bo[i][j]) + " ")
+
+
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)  # row, col
+
+    return None
+
+
+def solve(bo):
+    print(bo)
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+
+    return True
+
+
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                sys.stdout.write(" | ")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                sys.stdout.write(str(bo[i][j]) + " ")
+
+
+print('---------------------------------')
+print('---------before--solving---------')
+print('---------------------------------')
+print_board(board)
+
+solve(board)
+
+print('---------------------------------')
+print('---------after--solving----------')
+print('---------------------------------')
+print_board(board)
+
+
+
